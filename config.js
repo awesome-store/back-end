2651e14d303d25d6603f44a0cd0e73a3613b0249
Heorhii Siburov <g.siburov@gmail.com>
2020-05-14 11:29:05 -0700

    switch tool updated

--- config.js
+++ config.js
@@ -5,22 +5,18 @@ const isProduction = process.env.NODE_ENV === 'production'
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
-
 const pool = new Pool(getPool())
 
+// Checking location of app and using SSL rejectUnauthorized just in case of production
 function getPool() {
+  newPool = { connectionString: isProduction ? process.env.DATABASE_URL : connectionString }
   if (process.env.NODE_ENV === 'production') {
-    newPool = {
-      connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-      ssl: {rejectUnauthorized: false}
-    }
+    newPool.ssl = {rejectUnauthorized: false}
     return newPool 
-} else {
-    newPool = {
-      connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-      ssl: null
-    }
+  } else {
+    newPool.ssl = null
     return newPool 
-}}
+  }
+}
 
 module.exports = { pool }

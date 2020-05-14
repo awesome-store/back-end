6931c95714cbdf68915b2641579acc141914c0f7
Heorhii Siburov <g.siburov@gmail.com>
2020-05-14 11:21:33 -0700

    added switch to internal db

--- config.js
+++ config.js
@@ -5,11 +5,22 @@ const isProduction = process.env.NODE_ENV === 'production'
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
-const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: {
-    rejectUnauthorized: false
-  }
-})
+
+const pool = new Pool(getPool())
+
+function getPool() {
+  if (process.env.NODE_ENV === 'production') {
+    newPool = {
+      connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+      ssl: {rejectUnauthorized: false}
+    }
+    return newPool 
+} else {
+    newPool = {
+      connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+      ssl: null
+    }
+    return newPool 
+}}
 
 module.exports = { pool }
